bab4309ef55f47586699a960cf24c61788e3f8df
Al-Bsharat <qaisbsharat@onedrive.web.id>
2019-08-15 11:53:31 +0300

    Update Database connection infromation Docker-compose.yml

--- orders/config.py
+++ orders/config.py
@@ -1,3 +1,3 @@
 PROPAGATE_EXCEPTIONS = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:root@localhost:5432/orders_micro'
+SQLALCHEMY_DATABASE_URI = {'postgres://postgres:root@localhost:5432/orders_micro'}
