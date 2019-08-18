4937275d5fb2898c0d9c0ed586d1a4b4ec6bb103
Al-Bsharat <qaisbsharat@onedrive.web.id>
2019-08-18 12:12:46 +0300

    Added JWT_EXTENDED And Configure Order Service Inside docker-compose.yml

--- orders/app.py
+++ orders/app.py
@@ -3,19 +3,21 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 from db import db
 from resources.order import OrderResource
 
 app = Flask(__name__)
 api = Api(app)
+jwt = JWTManager()
 migrate = Migrate()
 
-app.secret_key = os.environ['SECRET_KEY']
 app.config.from_pyfile('config.py')
 
 db.init_app(app)
 migrate.init_app(app, db)
+jwt.init_app(app)
 
 api.add_resource(OrderResource, '/order')
 
